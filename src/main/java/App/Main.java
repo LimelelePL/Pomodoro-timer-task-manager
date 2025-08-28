a373a3e7501371bd968222c0900cf98d854e77c7
Dominik <DudekDominik80@gmail.com>
2025-08-28 13:17:59 +0200

    learning javaFX- added icon, stage, fullscreen

--- src/main/java/App/Main.java
+++ src/main/java/App/Main.java
@@ -1,7 +1,33 @@
 package App;
 
-public class Main{
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args){
+      launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Group root = new Group();
+        Scene scene = new Scene(root, Color.ROSYBROWN);
+        stage.setTitle("Pomodoro_TaskList");
+
+        Image icon= new Image("pomidor.png");
+        stage.getIcons().add(icon);
+
+        stage.setWidth(1000);
+        stage.setHeight(700);
+        stage.setResizable(false);
+
+        stage.setFullScreen(true);
+
+        stage.setScene(scene);
+        stage.show();
     }
 }
